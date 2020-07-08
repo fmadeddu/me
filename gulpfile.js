a872734219ae5ff0d02071badb94d99535ecbd61
Francesca M <francesca.madeddu@flexys.co.uk>
2020-07-08 17:30:01 +0100

    Fix project and update content

--- gulpfile.js
+++ gulpfile.js
@@ -20,39 +20,39 @@ const banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
+gulp.task('vendor', function (cb) {
 
   // Bootstrap
   gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
+    './node_modules/bootstrap/dist/**/*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+    '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  ])
     .pipe(gulp.dest('./vendor/bootstrap'))
 
   // Font Awesome
   gulp.src([
-      './node_modules/@fortawesome/**/*',
-    ])
+    './node_modules/@fortawesome/**/*',
+  ])
     .pipe(gulp.dest('./vendor'))
 
   // jQuery
   gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
     .pipe(gulp.dest('./vendor/jquery'))
 
   // jQuery Easing
   gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
+    './node_modules/jquery.easing/*.js'
+  ])
     .pipe(gulp.dest('./vendor/jquery-easing'))
 
   // Magnific Popup
   gulp.src([
-      './node_modules/magnific-popup/dist/*'
-    ])
+    './node_modules/magnific-popup/dist/*'
+  ])
     .pipe(gulp.dest('./vendor/magnific-popup'))
 
   cb();
